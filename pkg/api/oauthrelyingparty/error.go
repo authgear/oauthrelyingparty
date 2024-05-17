ca784534b8b24b0fc37979110eddc9a812026462
Louis Chan <louischan@oursky.com>
2024-05-17 15:41:16 +0800

    Make ErrorResponse an error

--- pkg/api/oauthrelyingparty/error.go
+++ pkg/api/oauthrelyingparty/error.go
@@ -1,7 +1,21 @@
 package oauthrelyingparty
 
+import (
+	"strings"
+)
+
 type ErrorResponse struct {
-	Error            string `json:"error"`
+	Error_           string `json:"error"`
 	ErrorDescription string `json:"error_description,omitempty"`
 	ErrorURI         string `json:"error_uri,omitempty"`
+}
+
+func (e *ErrorResponse) Error() string {
+	var buf strings.Builder
+	buf.WriteString(e.Error_)
+	if e.ErrorDescription != "" {
+		buf.WriteString(": ")
+		buf.WriteString(e.ErrorDescription)
+	}
+	return buf.String()
 }
